cd9035d8ab15efa2a285c14933d301234efe448b
TobiasWust <kontakt@tobiaswust.de>
2024-09-08 20:59:53 +0200

    fix hero heading

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,13 @@ export default function Header() {
         backgroundImage: "url('/headerimage.jpg')",
       }}>
       <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-neutral-content text-center">
-        <h1 className="mb-5 [font-size:_clamp(2em,5vw,10em)] font-bold">Gleitschirmjobs.de</h1>
-        <p className="mb-5">
-          Die erste Jobbörse für Gleitschirmflieger.
-        </p>
+      <div className="hero-content text-neutral-content column">
+        <div className="text-center">
+          <h1 className="mb-5 [font-size:_clamp(2em,5vw,10em)] font-bold">Gleitschirmjobs.de</h1>
+          <p className="mb-5">
+            Die erste Jobbörse für Gleitschirmflieger.
+          </p>
+        </div>
       </div>
     </header>
   );
