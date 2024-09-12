6e68af8ef590bb6f5d8680b7bdf7ee5b387596a0
TobiasWust <kontakt@tobiaswust.de>
2024-09-12 15:37:33 +0200

    fix lint

--- src/app/job/[id]/page.tsx
+++ src/app/job/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { HiBuildingOffice, HiOutlineArrowTopRightOnSquare, HiUser } from "react-icons/hi2";
 import { getCategoryNameById } from "../../../data/categories";
-import { createClient, getJob } from "../../../utils/supabase/server";
 import ApplyForm from "../../../components/ApplyForm";
+import { getJob } from "../../../utils/supabase/server";
 
 export default async function Job({ params: { id } }: { params: { id: string } }) {
   const job = await getJob(id);
