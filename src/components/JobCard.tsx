1c8b2a81531b179d4d36af9559c4b8384f05dc52
TobiasWust <kontakt@tobiaswust.de>
2024-09-17 23:03:04 +0200

    all but date

--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -1,40 +1,39 @@
 'use client';
-// import { HiBuildingOffice, HiUser } from "react-icons/hi2";
-// import { HiBuildingOffice, HiOutlineStar, HiStar, HiUser } from "react-icons/hi2";
-// import { useMemo } from "react"
-// import { getCategoryNameById } from "../data/categories";
-// import { useFav } from "../store/useFav";
-// import Link from "next/link";
+import { HiBuildingOffice, HiUser } from "react-icons/hi2";
+import { HiBuildingOffice, HiOutlineStar, HiStar, HiUser } from "react-icons/hi2";
+import { useMemo } from "react"
+import { getCategoryNameById } from "../data/categories";
+import { useFav } from "../store/useFav";
+import Link from "next/link";
 import { ClientJob } from "../types/supabaseTypes";
-// import slugify from "slugify";
+import slugify from "slugify";
 
 export default function JobCard({ job }: { job: ClientJob }) {
-  // const toggleFav = useFav((state) => state.toggleFav);
-  // const favs = useFav((state) => state.favs);
+  const toggleFav = useFav((state) => state.toggleFav);
+  const favs = useFav((state) => state.favs);
 
-  // const category = useMemo(() => getCategoryNameById(job.categoryId), [job.categoryId]);
+  const category = useMemo(() => getCategoryNameById(job.categoryId), [job.categoryId]);
 
-  // const handleFav = (e: React.MouseEvent) => {
-  //   e.stopPropagation();
-  //   e.preventDefault();
-  //   toggleFav(job.id);
-  // }
+  const handleFav = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    e.preventDefault();
+    toggleFav(job.id);
+  }
 
   return (
     <article>
-      {/* <Link href={`/job/${job.id}/${slugify(job.company)}-${slugify(job.title)}`}> */}
-      <div className={`card card-compact bg-base-200 w-full shadow-md hover:shadow-secondary transition-shadow`}>
-        {/* <div className={`card card-compact bg-base-200 w-full shadow-md ${job.highlight ? 'border-solid border-2 border-yellow-300' : ''} hover:shadow-secondary transition-shadow`}> */}
-        <div className="card-body">
-          <div className="flex justify-between">
-            <h2 className="card-title flex-wrap text-primary">
-              {/* {job.listingType === 'search' ?
-                <HiBuildingOffice /> :
-                <HiUser />
-              } */}
-              {job.title}<span className="badge badge-xs"></span></h2>
-            {/* {job.title}<span className="badge badge-xs">{category}</span></h2> */}
-            <div className="text-xs">{new Date(job.created_at).toLocaleDateString('de')}</div>
+      <Link href={`/job/${job.id}/${slugify(job.company)}-${slugify(job.title)}`}>
+        <div className={`card card-compact bg-base-200 w-full shadow-md ${job.highlight ? 'border-solid border-2 border-yellow-300' : ''} hover:shadow-secondary transition-shadow`}>
+          <div className="card-body">
+            <div className="flex justify-between">
+              <h2 className="card-title flex-wrap text-primary">
+                {job.listingType === 'search' ?
+                  <HiBuildingOffice /> :
+                  <HiUser />
+                }
+                {job.title}<span className="badge badge-xs"></span></h2>
+              {job.title}<span className="badge badge-xs">{category}</span></h2>
+            {/* <div className="text-xs">{new Date(job.created_at).toLocaleDateString('de')}</div> */}
           </div>
           <div className="flex gap-4">
             <h3 className="font-semibold text-primary">{job.company}</h3>
@@ -46,7 +45,7 @@ export default function JobCard({ job }: { job: ClientJob }) {
           <div className="card-actions justify-between items-center">
             <div className="card-actions">
               {
-                // job.employmentType?.map((type) => <div className="badge badge-outline" key={type}>{type}</div>)
+                job.employmentType?.map((type) => <div className="badge badge-outline" key={type}>{type}</div>)
               }
             </div>
             <div className="card-actions justify-end flex-1">
@@ -56,8 +55,7 @@ export default function JobCard({ job }: { job: ClientJob }) {
             </div>
           </div>
         </div>
-      </div>
-      {/* </Link> */}
+      </Link>
     </article >
   )
 }
