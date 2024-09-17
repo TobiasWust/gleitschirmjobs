4286b85851bb5b37323d90c218a31c7f1301472c
TobiasWust <kontakt@tobiaswust.de>
2024-09-17 22:15:25 +0200

    no jobs bailout?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 import { getActiveJobs } from '../utils/supabase/server';
 import { Suspense } from "react";
-// import JobTable from "../components/JobTable";
+import JobTable from "../components/JobTable";
 import SearchBar from "../components/Searchbar";
 
 export default async function Home() {
   const jobs = await getActiveJobs();
 
+  if (!jobs) {
+    return <div>No jobs found</div>
+  }
+
   return (
     <main>
       <SearchBar />
       <Suspense>
-        {JSON.stringify(jobs)}
-        {/* <JobTable jobs={jobs || []} /> */}
+        <JobTable jobs={jobs} />
       </Suspense>
     </main>
   );
