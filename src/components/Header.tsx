626ce0b4d7701ea11c2b5af1d0d303cd68e7d531
TobiasWust <kontakt@tobiaswust.de>
2024-09-27 13:22:35 +0200

    make header text clickable

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header>
       <div className="text-center">
-        <h1 className="mt-4 [font-size:_clamp(2em,4vw,10em)] leading-none">
-          Gleitschirmjobs</h1>
+        <Link href="/"><h1 className="mt-4 [font-size:_clamp(2em,4vw,10em)] leading-none">
+          Gleitschirmjobs</h1></Link>
         <p className="mb-4">
           Die Jobbörse für Gleitschirmfliegende.
         </p>
